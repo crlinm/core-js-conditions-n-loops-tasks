8ae1b4c1c0fca022d9adf747f2579d91f6bc7077
Alina M <crlin.m@gmail.com>
2024-12-08 03:06:02 +0500

    ref: refactor firstTenRoman (11/19)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -121,37 +121,19 @@ function convertToRomanNumerals(num) {
   for (let i = 0; i < tens; i += 1) {
     res += 'X';
   }
-  switch (num % 10) {
-    case 1:
-      res += 'I';
-      break;
-    case 2:
-      res += 'II';
-      break;
-    case 3:
-      res += 'III';
-      break;
-    case 4:
-      res += 'IV';
-      break;
-    case 5:
-      res += 'V';
-      break;
-    case 6:
-      res += 'VI';
-      break;
-    case 7:
-      res += 'VII';
-      break;
-    case 8:
-      res += 'VIII';
-      break;
-    case 9:
-      res += 'IX';
-      break;
-    default:
-      break;
-  }
+  const firstTenRoman = [
+    '',
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX',
+  ];
+  res += firstTenRoman[num % 10];
   return res;
 }
 
